162ec07006e11b18f4afaff03e6510121cb5303f
JigneshMahadik <jigneshmahadik2898@gmail.com>
2024-01-12 17:31:12 +0530

    DOM Project 1

---
+++ script.js
@@ -0,0 +1,19 @@
+let div = document.getElementById("div1");
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize  = "18px";
+div.style.fontWeight = "bold";
+div.style.width = "300px";
+div.style.height = "200px";
+div.style.color = "blue";
+
+
+// Add Event Listener
+
+// function clickDiv(e){
+//     console.log("Div is clicked",e);
+//     div.style.backgroundColor = "pink";
+// }
+
+// div.addEventListener("click", clickDiv);
